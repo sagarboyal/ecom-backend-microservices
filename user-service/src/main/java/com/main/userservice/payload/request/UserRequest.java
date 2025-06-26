9dad2806f784a788329652ee1caba07a1a6d37dc
sagarboyal <sboyal12@gmail.com>
2025-06-26 19:53:04 +0530

    [user-service]: add new request handler for user

---
+++ user-service/src/main/java/com/main/userservice/payload/request/UserRequest.java
@@ -0,0 +1,16 @@
+package com.main.userservice.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequest {
+    private String userName;
+    private String email;
+    private String password;
+}
