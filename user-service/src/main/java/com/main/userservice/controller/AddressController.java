84f862fd94824086581b529e8af2c4336018c75f
sagarboyal <sboyal12@gmail.com>
2025-06-26 19:49:39 +0530

    [user-service]: add required controllers!

---
+++ user-service/src/main/java/com/main/userservice/controller/AddressController.java
@@ -0,0 +1,61 @@
+package com.main.userservice.controller;
+
+import com.main.userservice.model.User;
+import com.main.userservice.payload.dtos.AddressDTO;
+import com.main.userservice.service.AddressService;
+import com.main.userservice.utils.AuthUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/addresses")
+@RequiredArgsConstructor
+public class AddressController {
+    private final AddressService addressService;
+    private AuthUtil authUtil;
+
+
+    @GetMapping
+    public ResponseEntity<List<AddressDTO>> getAddressHandler(){
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(addressService.getAddressList());
+    }
+
+    @GetMapping("/{addressId}")
+    public ResponseEntity<AddressDTO> getAddressHandler(@PathVariable Long addressId){
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(addressService.getAddressList(addressId));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<AddressDTO>> getUserAddressHandler(){
+        User user = authUtil.loggedInUser(null);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(addressService.getUserAddressList(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressDTO> saveAddressHandler(@Valid @RequestBody AddressDTO addressDTO) {
+        User user = authUtil.loggedInUser(null);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(addressService.saveAddress(addressDTO, user));
+    }
+
+    @PutMapping("/{addressId}")
+    public ResponseEntity<AddressDTO> updateAddressHandler(@Valid @RequestBody AddressDTO addressDTO,
+                                                           @PathVariable Long addressId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(addressService.updateAddress(addressId, addressDTO));
+    }
+
+    @DeleteMapping("/{addressId}")
+    public ResponseEntity<AddressDTO> deleteAddressHandler(@PathVariable Long addressId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(addressService.deleteAddress(addressId));
+    }
+}
---
+++ user-service/src/main/java/com/main/userservice/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.main.userservice.controller;
+import com.main.userservice.constraints.Constraints;
+import com.main.userservice.payload.request.UserRequest;
+import com.main.userservice.payload.response.PagedResponse;
+import com.main.userservice.payload.response.UserResponse;
+import com.main.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
+        return ResponseEntity.ok(userService.createUser(userRequest));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedResponse<UserResponse>> getAllUsers(
+            @RequestParam(required = false, defaultValue = Constraints.PAGE_NUMBER) Integer pageNumber,
+            @RequestParam(required = false, defaultValue = Constraints.PAGE_SIZE) Integer pageSize
+    ) {
+        return ResponseEntity.ok(userService.getAllUser(pageNumber, pageSize));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UserRequest request) {
+        return ResponseEntity.ok(userService.updateUser(id, request));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully.");
+    }
+}
+
