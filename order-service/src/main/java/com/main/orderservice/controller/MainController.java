ee7cf72373904ee62e5d8a467a90e2e2e5eb4ce0
sagarboyal <sboyal12@gmail.com>
2025-06-29 10:12:54 +0530

    [order-service]: add actuator, lombok, web, config-client, spring-cloud-bus dependency and configured!

---
+++ order-service/src/main/java/com/main/orderservice/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.main.orderservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Hello World from order service");
+    }
+}
