dcfaf46943f5d9d6293942befdfda56fc4485acd
sagarboyal <sboyal12@gmail.com>
2025-06-29 09:55:20 +0530

    [product-service]: add spring cloud bus dependency and configure all properties!

---
+++ product-service/src/main/java/com/main/productservice/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.main.productservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Hello World from product service");
+    }
+}
