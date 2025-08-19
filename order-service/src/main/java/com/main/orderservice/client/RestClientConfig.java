fceb9821c28f54373dd0dc0f4acaf904e44d196f
sagarboyal <sboyal12@gmail.com>
2025-08-19 19:52:28 +0530

    chore(tracing): configure Zipkin for distributed tracing in order-service
    - Added Micrometer Tracing with Brave and Zipkin reporter
    - Configured tracing sampling probability (100% for dev)
    - Added RestClient interceptor for trace context propagation
    - Enabled service-to-service tracing for Zipkin UI visualization

--- order-service/src/main/java/com/main/orderservice/client/RestClientConfig.java
+++ order-service/src/main/java/com/main/orderservice/client/RestClientConfig.java
@@ -1,15 +1,50 @@
 package com.main.orderservice.client;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Configuration
 public class RestClientConfig {
+    private final Optional<ObservationRegistry> observationRegistry;
+    private final Optional<Tracer> tracer;
+    private final Optional<Propagator> propagator;
+
+    public RestClientConfig(Optional<ObservationRegistry> observationRegistry,
+                            Optional<Tracer> tracer,
+                            Optional<Propagator> propagator) {
+        this.observationRegistry = observationRegistry;
+        this.tracer = tracer;
+        this.propagator = propagator;
+    }
+
     @Bean
     @LoadBalanced
     public RestClient.Builder restTemplate() {
-        return RestClient.builder();
+        RestClient.Builder  clientBuilder = RestClient.builder();
+        if (observationRegistry.isPresent()) clientBuilder.requestInterceptor(tracingInterceptor());
+        return clientBuilder;
+    }
+
+    private ClientHttpRequestInterceptor tracingInterceptor() {
+        return ((request, body, execution) -> {
+            if(tracer.isPresent() && propagator.isPresent() && tracer.get().currentSpan() != null) {
+                propagator.get().inject(Objects.requireNonNull(tracer.get().currentTraceContext().context()),
+                        request.getHeaders(),
+                        (carrier, key, value) -> {
+                            assert carrier != null;
+                            carrier.add(key, value);
+                        });
+            }
+            return execution.execute(request, body);
+        });
     }
 }
